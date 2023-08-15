0fe0a3f520ce92d0619d60f58249e4a1b0a596d7
ranitsarker <rntprince@gmail.com>
2023-08-15 16:38:35 +0600

    I have connect index file with script js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 document.getElementById('login').addEventListener('click', function(){
-    const mailField = document.getElementById('mail');
-    const mail = mailField.value;
+    const mailField = document.getElementById('email');
+    const email = mailField.value;
 
     const passwordField = document.getElementById('password');
     const password = passwordField.value;
     // console.log(mail, password);
 
-    if(mail === 'rntprince@gmail.com' && password === '123456'){
+    if(email === 'rntprince@gmail.com' && password === '123456'){
         window.location.href = 'bank.html';
     }else{
-        console.log('invalid user');
+        alert('invalid user');
     }
 });
